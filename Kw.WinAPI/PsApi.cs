f1e1137d9ddc204f84124db5893fe2fe6feb2c20
ramayasket <as@andrique.ru>
2018-06-13 16:18:52 +0300

    PsApi

---
+++ Kw.WinAPI/PsApi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kw.WinAPI
+{
+	public static class PsApi
+	{
+		[DllImport("psapi.dll")]
+		public static extern uint GetProcessImageFileName(IntPtr hProcess, [Out] StringBuilder lpImageFileName, [In] [MarshalAs(UnmanagedType.U4)] int nSize);
+	}
+}
