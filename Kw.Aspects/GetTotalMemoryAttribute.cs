03b61dc391afefc4d0c7a59be05cad70b10e57ba
ramayasket <as@andrique.ru>
2019-06-16 11:59:58 +0300

    Kw.Aspects 1.0.2.7 -- SingleInstanceAttribute

---
+++ Kw.Aspects/GetTotalMemoryAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using PostSharp.Aspects;
+
+namespace Kw.Aspects
+{
+	/// <summary>
+	/// Cleans up memory upon executing attributed method.
+	/// </summary>
+	[Serializable]
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+	public class GetTotalMemoryAttribute : MethodInterceptionAspect
+	{
+		public sealed override void OnInvoke(MethodInterceptionArgs args)
+		{
+			args.Proceed();
+
+			GC.GetTotalMemory(true);
+		}
+	}
+}
--- Kw.Aspects/Properties/AssemblyInfo.cs
+++ Kw.Aspects/Properties/AssemblyInfo.cs
@@ -8,9 +8,11 @@ using System.Runtime.InteropServices;
 	Date		Version		Comments
 	===============================================================================
 	04.05.2019	1.0.2.5		PostSharp 6.2.4-rc
+	24.05.2019	1.0.2.6		PostSharp 6.2.6 + GetTotalMemoryAttribute
+	16.06.2019	1.0.2.7		SingleInstanceAttribute
 */
 
 [assembly: AssemblyTitle("Kw.Aspects")]
-[assembly: AssemblyVersion("1.0.2.5")]
+[assembly: AssemblyVersion("1.0.2.7")]
 [assembly: AssemblyDescription("All purpose aspects")]
 [assembly: Guid("00000000-0002-11e8-9876-00055d74a52d")]
